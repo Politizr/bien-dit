ed1f3fe5b5e8f5c8fea70eee4aadb7b19d116d42
Lionel Bouzonville <lionel.bouzonville@gmail.com>
2015-07-18 13:47:02 +0200

    minor fix

--- src/Politizr/FrontBundle/Twig/PolitizrDocumentExtension.php
+++ src/Politizr/FrontBundle/Twig/PolitizrDocumentExtension.php
@@ -179,7 +179,7 @@ class PolitizrDocumentExtension extends \Twig_Extension
                     $uploadWebPath = PathConstants::DEBATE_UPLOAD_WEB_PATH;
                     break;
                 case ObjectTypeConstants::TYPE_REACTION:
-                    $uploadWebPath = PDReaction::UPLOAD_WEB_PATH;
+                    $uploadWebPath = PDReaction::REACTION_UPLOAD_WEB_PATH;
                     break;
             }
 
