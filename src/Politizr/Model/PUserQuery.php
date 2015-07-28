ec5aad899d1cb77a5a673f94fb737a809a8c881a
Lionel Bouzonville <lionel.bouzonville@gmail.com>
2015-07-28 11:17:19 +0200

    SQL user request suggestion strange bug @todo find a fix

--- src/Politizr/Model/PUserQuery.php
+++ src/Politizr/Model/PUserQuery.php
@@ -24,6 +24,8 @@ class PUserQuery extends BasePUserQuery
      *
      * @todo:
      *   > + suggestions depuis les users déjà suivis
+     *   > BUG sous requête / renvoit potentiellement une ligne avec null partout!
+     *   SELECT p_user.*, COUNT(p_user.id) as nb_users, 1 as unionsorting FROM p_user LEFT JOIN p_u_tagged_t ON p_user.id = p_u_tagged_t.p_user_id WHERE p_u_tagged_t.p_tag_id IN ( SELECT p_tag.id FROM p_tag LEFT JOIN p_u_follow_t ON p_tag.id = p_u_follow_t.p_tag_id WHERE p_tag.online = true AND p_u_follow_t.p_user_id = 73 ) AND p_user.online = 1 AND p_user.id NOT IN (SELECT p_user_id FROM p_u_follow_u WHERE p_user_id = 73) AND p_user.id <> 73
      *
      * @param  integer     $userId
      * @param  integer     $offset
