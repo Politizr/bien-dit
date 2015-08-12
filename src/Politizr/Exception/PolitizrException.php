0f4394b8da1f81e40713ae4a5bece8a337d8be60
Lionel Bouzonville <lionel.bouzonville@gmail.com>
2015-08-12 17:02:19 +0200

    Fix signature bug

--- src/Politizr/Exception/PolitizrException.php
+++ src/Politizr/Exception/PolitizrException.php
@@ -9,7 +9,7 @@ namespace Politizr\Exception;
  */
 class PolitizrException extends \Exception implements PolitizrExceptionInterface
 {
-    public function __construct($message = null, \Exception $previous = null, $code = 0)
+    public function __construct($message = null, $code = 0, \Exception $previous = null)
     {
         parent::__construct($message, $code, $previous);
     }
