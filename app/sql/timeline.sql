f161e3ef184cb71990c342f34018bd725302413e
Lionel Bouzonville <lionel.bouzonville@gmail.com>
2015-10-21 10:16:05 +0200

    Fix #63

--- app/sql/timeline.sql
+++ app/sql/timeline.sql
@@ -1,3 +1,24 @@
+#  Débats publiés
+( SELECT p_d_debate.id as id, p_d_debate.title as title, p_d_debate.published_at as published_at, 'Politizr\\Model\\PDDebate' as type
+FROM p_d_debate
+WHERE
+    p_d_debate.published = 1
+    AND p_d_debate.online = 1
+    AND p_d_debate.p_user_id = 1 )
+
+UNION DISTINCT
+
+#  Réactions publiés
+( SELECT p_d_reaction.id as id, p_d_reaction.title as title, p_d_reaction.published_at as published_at, 'Politizr\\Model\\PDReaction' as type
+FROM p_d_reaction
+WHERE
+    p_d_reaction.published = 1
+    AND p_d_reaction.online = 1
+    AND p_d_reaction.p_user_id = 1
+    AND p_d_reaction.tree_level > 0 )
+
+UNION DISTINCT
+
 #  Débats suivis
 ( SELECT p_d_debate.id as id, p_d_debate.title as title, p_d_debate.published_at as published_at, 'Politizr\\Model\\PDDebate' as type
 FROM p_d_debate
--- src/Politizr/FrontBundle/Lib/Manager/UserManager.php
+++ src/Politizr/FrontBundle/Lib/Manager/UserManager.php
@@ -74,6 +74,25 @@ FROM p_d_debate
 WHERE
     p_d_debate.published = 1
     AND p_d_debate.online = 1
+    AND p_d_debate.p_user_id = ".$userId." )
+
+UNION DISTINCT
+
+( SELECT p_d_reaction.id as id, p_d_reaction.title as title, p_d_reaction.published_at as published_at, 'Politizr\\\Model\\\PDReaction' as type
+FROM p_d_reaction
+WHERE
+    p_d_reaction.published = 1
+    AND p_d_reaction.online = 1
+    AND p_d_reaction.p_user_id = ".$userId."
+    AND p_d_reaction.tree_level > 0 )
+
+UNION DISTINCT
+
+( SELECT p_d_debate.id as id, p_d_debate.title as title, p_d_debate.published_at as published_at, 'Politizr\\\Model\\\PDDebate' as type
+FROM p_d_debate
+WHERE
+    p_d_debate.published = 1
+    AND p_d_debate.online = 1
     AND p_d_debate.id IN (".$inQueryDebateIds.") )
 
 UNION DISTINCT
