b8d2080a0ae2432c78e389fccb2d78626534ea84
Lionel Bouzonville <lionel.bouzonville@gmail.com>
2015-11-03 15:30:25 +0100

    Fix #43

--- src/Politizr/AdminBundle/Controller/PQOrganization/ActionsController.php
+++ src/Politizr/AdminBundle/Controller/PQOrganization/ActionsController.php
@@ -9,4 +9,47 @@ use Admingenerated\PolitizrAdminBundle\BasePQOrganizationController\ActionsContr
  */
 class ActionsController extends BaseActionsController
 {
+    /**
+     *
+     */
+    protected function executeObjectMoveUp(\Politizr\Model\PQOrganization $PQOrganization)
+    {
+        $PQOrganization->moveUp();
+        $PQOrganization->save();
+    }
+
+    /**
+     *
+     */
+    protected function successObjectMoveup(\Politizr\Model\PQOrganization $PQOrganization)
+    {
+        $this->get('session')->getFlashBag()->add(
+            'success',
+            'La position a été mise à jour avec succès'
+        );
+
+        return new \Symfony\Component\HttpFoundation\RedirectResponse($this->generateUrl("Politizr_AdminBundle_PQOrganization_list"));
+    }
+    
+    /**
+     *
+     */
+    protected function executeObjectMoveDown(\Politizr\Model\PQOrganization $PQOrganization)
+    {
+        $PQOrganization->moveDown();
+        $PQOrganization->save();
+    }
+    
+    /**
+     *
+     */
+    protected function successObjectMovedown(\Politizr\Model\PQOrganization $PQOrganization)
+    {
+        $this->get('session')->getFlashBag()->add(
+            'success',
+            'La position a été mise à jour avec succès'
+        );
+
+        return new \Symfony\Component\HttpFoundation\RedirectResponse($this->generateUrl("Politizr_AdminBundle_PQOrganization_list"));
+    }
 }
