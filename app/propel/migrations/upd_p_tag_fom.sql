0752ef7a6233f93d9d9d864293ce82d99d330ec9
Lionel Bouzonville <lionel.bouzonville@gmail.com>
2015-11-25 18:01:57 +0100

    WIP #115 > upd label & remove sql upd

--- app/propel/migrations/upd_p_tag_fom.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO `p_tag` (`id`, `p_t_tag_type_id`, `p_t_parent_id`, `title`, `online`, `created_at`, `updated_at`, `slug`)
-VALUES
-(124, 1, 22, 'Polynésie française', 1, NOW(), NOW(), 'polynesie-francaise'),
-(125, 1, 22, 'Saint-Barthélemy', 1, NOW(), NOW(), 'saint-barthelemy'),
-(126, 1, 22, 'Saint-Martin', 1, NOW(), NOW(), 'saint-martin'),
-(127, 1, 22, 'Saint-Pierre-et-Miquelon', 1, NOW(), NOW(), 'saint-pierre-et-miquelon'),
-(128, 1, 22, 'Wallis-et-Futuna', 1, NOW(), NOW(), 'wallis-et-futuna'),
-(129, 1, 22, 'Nouvelle-Calédonie', 1, NOW(), NOW(), 'nouvelle-caledonie')
-;
