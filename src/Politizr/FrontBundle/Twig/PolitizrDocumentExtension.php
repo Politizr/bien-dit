254dc3114b5cd9b0964ffb429a2142dd79d81dbd
Lionel Bouzonville <lionel.bouzonville@gmail.com>
2015-11-30 10:33:08 +0100

    Fix #153

--- src/Politizr/FrontBundle/Twig/PolitizrDocumentExtension.php
+++ src/Politizr/FrontBundle/Twig/PolitizrDocumentExtension.php
@@ -85,6 +85,11 @@ class PolitizrDocumentExtension extends \Twig_Extension
                 array('is_safe' => array('html'))
             ),
             new \Twig_SimpleFilter(
+                'nbViews',
+                array($this, 'nbViews'),
+                array('is_safe' => array('html'))
+            ),
+            new \Twig_SimpleFilter(
                 'nbReactions',
                 array($this, 'nbReactions'),
                 array('is_safe' => array('html'))
@@ -233,6 +238,29 @@ class PolitizrDocumentExtension extends \Twig_Extension
         return $html;
     }
 
+    /**
+     * Document's number of views
+     *
+     * @param PDocumentInterface $document
+     * @return html
+     */
+    public function nbViews(PDocumentInterface $document)
+    {
+        // $this->logger->info('*** nbViews');
+        // $this->logger->info('$document = '.print_r($document, true));
+
+        $nbViews = $document->getNbViews();
+
+        if (0 === $nbViews) {
+            $html = 'Aucune vue';
+        } elseif (1 === $nbViews) {
+            $html = '1 vue';
+        } else {
+            $html = $nbViews.' vues';
+        }
+
+        return $html;
+    }
 
     /**
      * Document's number of reactions
