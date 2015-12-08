641329aaf3651e9a2004c5580efc4181984d0586
Lionel Bouzonville <lionel.bouzonville@gmail.com>
2015-12-08 15:31:45 +0100

    WIP #265 > PUTaggedT & PDDTaggedT

--- src/Politizr/Model/PDDTaggedT.php
+++ src/Politizr/Model/PDDTaggedT.php
@@ -2,9 +2,6 @@
 
 namespace Politizr\Model;
 
-use Symfony\Component\DependencyInjection\ContainerAwareInterface;
-use Symfony\Component\DependencyInjection\ContainerInterface;
-
 use Politizr\Model\om\BasePDDTaggedT;
 
 /**
@@ -12,62 +9,6 @@ use Politizr\Model\om\BasePDDTaggedT;
  *
  * @author Lionel Bouzonville
  */
-class PDDTaggedT extends BasePDDTaggedT implements ContainerAwareInterface
+class PDDTaggedT extends BasePDDTaggedT
 {
-    private $elasticaPersister;
-
-    /**
-     * @param ContainerInterface $container
-     */
-    public function setContainer(ContainerInterface $container = null)
-    {
-        if ($container) {
-            $this->elasticaPersister = $container->get('fos_elastica.object_persister.politizr.p_d_debate');
-        }
-    }
-
-    /**
-     * @todo: gestion d'une exception spécifique à ES
-     */
-    public function postInsert(\PropelPDO $con = null)
-    {
-        if ($this->elasticaPersister) {
-            $debate = PDDebateQuery::create()->findPk($this->getPDDebateId());
-            if ($debate && $debate->isIndexable()) {
-                // $this->elasticaPersister->replaceOne($debate);
-            }
-        } else {
-            throw new \Exception('Indexation service not found');
-        }
-    }
-
-    /**
-     * @todo: gestion d'une exception spécifique à ES
-     */
-    public function postUpdate(\PropelPDO $con = null)
-    {
-        if ($this->elasticaPersister) {
-            $debate = PDDebateQuery::create()->findPk($this->getPDDebateId());
-            if ($debate && $debate->isIndexable()) {
-                // $this->elasticaPersister->replaceOne($debate);
-            }
-        } else {
-            throw new \Exception('Indexation service not found');
-        }
-    }
-
-    /**
-     * @todo: gestion d'une exception spécifique à ES
-     */
-    public function postDelete(\PropelPDO $con = null)
-    {
-        if ($this->elasticaPersister) {
-            $debate = PDDebateQuery::create()->findPk($this->getPDDebateId());
-            if ($debate && $debate->isIndexable()) {
-                // $this->elasticaPersister->replaceOne($debate);
-            }
-        } else {
-            throw new \Exception('Indexation service not found');
-        }
-    }
 }
--- src/Politizr/Model/PUTaggedT.php
+++ src/Politizr/Model/PUTaggedT.php
@@ -2,78 +2,12 @@
 
 namespace Politizr\Model;
 
-use Symfony\Component\DependencyInjection\ContainerAwareInterface;
-use Symfony\Component\DependencyInjection\ContainerInterface;
-
 use Politizr\Model\om\BasePUTaggedT;
 
 /**
  *
  * @author Lionel Bouzonville
  */
-class PUTaggedT extends BasePUTaggedT implements ContainerAwareInterface
+class PUTaggedT extends BasePUTaggedT
 {
-    // elastica search
-    private $elasticaPersister;
-
-    /**
-     * @param ContainerInterface $container
-     */
-    public function setContainer(ContainerInterface $container = null)
-    {
-        if ($container) {
-            $this->elasticaPersister = $container->get('fos_elastica.object_persister.politizr.p_user');
-        }
-    }
-
-    /**
-     * @todo: gestion d'une exception spécifique à ES
-     *
-     */
-    public function postInsert(\PropelPDO $con = null)
-    {
-        if ($this->elasticaPersister) {
-            // get associated object
-            $user = PUserQuery::create()->findPk($this->getPUserId());
-            if ($user && $user->isIndexable()) {
-                // $this->elasticaPersister->replaceOne($user);
-            }
-        } else {
-            throw new \Exception('Indexation service not found');
-        }
-    }
-
-    /**
-     * @todo: gestion d'une exception spécifique à ES
-     *
-     */
-    public function postUpdate(\PropelPDO $con = null)
-    {
-        if ($this->elasticaPersister) {
-            // get associated object
-            $user = PUserQuery::create()->findPk($this->getPUserId());
-            if ($user && $user->isIndexable()) {
-                // $this->elasticaPersister->replaceOne($user);
-            }
-        } else {
-            throw new \Exception('Indexation service not found');
-        }
-    }
-
-    /**
-     * @todo: gestion d'une exception spécifique à ES
-     *
-     */
-    public function postDelete(\PropelPDO $con = null)
-    {
-        if ($this->elasticaPersister) {
-            // get associated object
-            $user = PUserQuery::create()->findPk($this->getPUserId());
-            if ($user && $user->isIndexable()) {
-                // $this->elasticaPersister->replaceOne($user);
-            }
-        } else {
-            throw new \Exception('Indexation service not found');
-        }
-    }
 }
