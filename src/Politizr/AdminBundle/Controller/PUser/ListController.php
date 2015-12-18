db12d6cc377d1313f1bae2d8b0e30d82202778e1
Lionel Bouzonville <lionel.bouzonville@gmail.com>
2015-12-18 13:58:20 +0100

    composer update

--- src/Politizr/AdminBundle/Controller/PUser/ListController.php
+++ src/Politizr/AdminBundle/Controller/PUser/ListController.php
@@ -9,4 +9,29 @@ use Admingenerated\PolitizrAdminBundle\BasePUserController\ListController as Bas
  */
 class ListController extends BaseListController
 {
+    /**
+     * Add the filters to the query for created_at
+     *
+     * @param queryFilter The queryFilter
+     * @param mixed The value
+     */
+    protected function filterCreatedAt($queryFilter, $value)
+    {
+        $filterObject = $this->getFilters();
+
+        $queryFilter->addDateRangeFilter('created_at', $filterObject['created_at']);
+    }
+
+    /**
+     * Add the filters to the query for created_at
+     *
+     * @param queryFilter The queryFilter
+     * @param mixed The value
+     */
+    protected function filterLastActivity($queryFilter, $value)
+    {
+        $filterObject = $this->getFilters();
+
+        $queryFilter->addDateRangeFilter('last_activity', $filterObject['last_activity']);
+    }
 }
