3a535af4b44a33d243cbdffca5c71d8e8a00b582
Lionel Bouzonville <lionel.bouzonville@gmail.com>
2015-12-29 15:09:05 +0100

    Admin > edit PUser > fix a bug in password upd

--- src/Politizr/AdminBundle/Controller/PUser/EditController.php
+++ src/Politizr/AdminBundle/Controller/PUser/EditController.php
@@ -4,10 +4,25 @@ namespace Politizr\AdminBundle\Controller\PUser;
 
 use Admingenerated\PolitizrAdminBundle\BasePUserController\EditController as BaseEditController;
 
+use Symfony\Component\Form\Form;
+
+use Politizr\Model\PUser;
+
 /**
  * EditController
  */
 class EditController extends BaseEditController
 {
+    /**
+     * @see Admingenerated\PolitizrAdminBundle\BasePUserController\EditController
+     */
+    protected function preSave(Form $form, PUser $user)
+    {
+        $userManager = $this->get('politizr.manager.user');
 
+        $userManager->updateCanonicalFields($user);
+        if ($user->getPlainPassword()) {
+            $userManager->updatePassword($user);
+        }
+    }
 }
