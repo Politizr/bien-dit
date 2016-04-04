8adbd55b8a93d8c155a0140540a47e7b05e29868
Lionel Bouzonville <lionel.bouzonville@gmail.com>
2016-04-04 12:05:09 +0200

    Fix #326

--- src/Politizr/FrontBundle/Resources/public/js/politizr/document/medium.js
+++ src/Politizr/FrontBundle/Resources/public/js/politizr/document/medium.js
@@ -3,25 +3,24 @@
 // description
 var descriptionEditor = new MediumEditor('.editable.description', {
     toolbar: {
-        buttons: ['bold', 'italic', 'header1', 'header2', 'unorderedlist', 'quote', 'anchor'],
-        buttonLabels: {
-            'bold': '<i class="icon-bold"></i>',
-            'italic': '<i class="icon-italic"></i>',
-            'header1': '<i class="icon-h1"></i>',
-            'header2': '<i class="icon-h2"></i>',
-            'unorderedlist': '<i class="icon-timeline"></i>',
-            'quote': '<i class="icon-quote"></i>',
-            'anchor': '<i class="icon-link"></i>'
-        },
-        placeholder: {
-            text: 'Cliquez ici pour saisir le texte de votre publication.'
-        },
-        anchor: {
-            placeholderText: 'Saississez une adresse internet'
-        },
-        anchorPreview: false,
-        firstHeader:'h1',
-        secondHeader:'h2',
-        autoLink: true
-    }
+        allowMultiParagraphSelection: false,
+        diffTop: -15,
+        buttons: [
+            {name: 'bold', contentDefault: '<i class="icon-bold"></i>'}, 
+            {name: 'italic', contentDefault: '<i class="icon-italic"></i>'}, 
+            {name: 'h1', contentDefault: '<i class="icon-h1"></i>'},
+            {name: 'h2', contentDefault: '<i class="icon-h2"></i>'},
+            {name: 'unorderedlist', contentDefault: '<i class="icon-timeline"></i>'},
+            {name: 'quote', contentDefault: '<i class="icon-quote"></i>'},
+            {name: 'anchor', contentDefault: '<i class="icon-link"></i>'}
+        ]
+    },
+    placeholder: {
+        text: 'Cliquez ici pour saisir le texte de votre publication.'
+    },
+    anchor: {
+        placeholderText: 'Saisissez une adresse internet'
+    },
+    anchorPreview: false,
+    autoLink:true
 });
