7718a9650d102e656f2b907ff80b95b8a921daa3
Lionel Bouzonville <lionel.bouzonville@gmail.com>
2016-04-12 11:35:59 +0200

    Fix #386

---
+++ src/Politizr/AdminBundle/Controller/PUser/ExcelController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Politizr\AdminBundle\Controller\PUser;
+
+use Admingenerated\PolitizrAdminBundle\BasePUserController\ExcelController as BaseExcelController;
+
+/**
+ * ExcelController
+ */
+class ExcelController extends BaseExcelController
+{
+}
