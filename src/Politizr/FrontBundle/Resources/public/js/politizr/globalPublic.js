bec4659b379b82e373a14a22fdfc249997e681fb
Lionel Bouzonville <lionel.bouzonville@gmail.com>
2016-06-23 11:03:21 +0200

    WIP upd home

--- src/Politizr/FrontBundle/Resources/public/js/politizr/globalPublic.js
+++ src/Politizr/FrontBundle/Resources/public/js/politizr/globalPublic.js
@@ -1,8 +1,6 @@
 // beta
 
-$(document).ready(function(){ $("#subForm").validate(); }); 
-
-jQuery.extend(jQuery.validator.messages, {
-    required: "Ce champ est obligatoire.",
-    email: "Merci de renseigner une adresse email valide."
+// scroll video en home
+$("body").on("click", "[action='scrollVideo']", function() {    
+    $('html, body').animate({ scrollTop: $('#videoFooter').offset().top +1}, '1000');
 });
