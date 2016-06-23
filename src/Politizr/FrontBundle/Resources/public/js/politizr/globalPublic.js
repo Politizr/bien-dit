a5a73f9e0580a7f16fb8a8565d1878ec6b1f3bbe
Lionel Bouzonville <lionel.bouzonville@gmail.com>
2016-06-23 13:42:54 +0200

    Fix public menu issue

--- src/Politizr/FrontBundle/Resources/public/js/politizr/globalPublic.js
+++ src/Politizr/FrontBundle/Resources/public/js/politizr/globalPublic.js
@@ -4,3 +4,17 @@
 $("body").on("click", "[action='scrollVideo']", function() {    
     $('html, body').animate({ scrollTop: $('#videoFooter').offset().top +1}, '1000');
 });
+
+// toggle menu
+$("body").on("mousedown touchstart", function(e) {
+    var container = $("body.css700 #headerMenu, #publicMenu, [action='toggleMenu']");
+    if (!container.is(e.target) // if the target of the click isn't the container...
+        && container.has(e.target).length === 0) // ... nor a descendant of the container
+    {
+        $('#publicMenu, body.css700 #headerMenu').hide();       
+    }
+});
+
+$("body").on("click", "[action='toggleMenu']", function() {
+    $('#publicMenu, body.css700 #headerMenu').toggle();
+});
--- src/Politizr/FrontBundle/Resources/public/js/politizr/pages/home.js
+++ src/Politizr/FrontBundle/Resources/public/js/politizr/pages/home.js
@@ -30,19 +30,6 @@ $(function(){
         verticalAlign: "center"
     });
 
-    // toggle menu
-    $("body").on("mousedown touchstart", function(e) {
-        var container = $("body.css700 #headerMenu, #publicMenu, [action='toggleMenu']");
-        if (!container.is(e.target) // if the target of the click isn't the container...
-            && container.has(e.target).length === 0) // ... nor a descendant of the container
-        {
-            $('#publicMenu, body.css700 #headerMenu').hide();       
-        }
-    });
-    $("body").on("click", "[action='toggleMenu']", function() {
-        $('#publicMenu, body.css700 #headerMenu').toggle();
-    });
-    
     // transparent header
     $.fn.isVisible = function() {    
         var rect = this[0].getBoundingClientRect();
