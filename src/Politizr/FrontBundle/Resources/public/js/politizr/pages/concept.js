0244b930890f1f7dc60915fe3f27a8bcf2f89244
Lionel Bouzonville <lionel.bouzonville@gmail.com>
2016-08-17 11:05:22 +0200

    data db upd #533

--- src/Politizr/FrontBundle/Resources/public/js/politizr/pages/concept.js
+++ src/Politizr/FrontBundle/Resources/public/js/politizr/pages/concept.js
@@ -1,12 +1,23 @@
-$(function(){
-    $(document)
-        .ready(function(e){
-            triggerTransparentHeader();
-        });
+$(document)
+    .ready(function(e){
+        triggerTransparentHeader();
+    });
 
-    $(window)
-        .scroll(function(e){
-            triggerTransparentHeader();   
-            $('#publicMenu, body.css700 #headerMenu').hide(); //hide menu au scroll
-        });
+$(window)
+    .scroll(function(e){
+        triggerTransparentHeader();   
+        $('#publicMenu, body.css700 #headerMenu').hide(); //hide menu au scroll
+    });
+
+// open video modal sur page concept
+$("body").on("click", "[action='openVideoModal']", function() {
+    $('body').addClass('noScroll');
+    $('#modalVideo').fadeIn();
 });
+
+// close modal 
+$("body").on("click", "[action='closeVideoModal']", function() {
+    $('body').removeClass('noScroll');
+    $('#modalVideo').hide();
+});     
+
