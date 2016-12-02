42e4ae6c7584aa6693e035aca1fc613b2f35e2e4
Lionel Bouzonville <lionel.bouzonville@gmail.com>
2016-12-02 12:16:54 +0100

    upd default routes definition - detail user & tag listing

--- src/Politizr/FrontBundle/Controller/ListingController.php
+++ src/Politizr/FrontBundle/Controller/ListingController.php
@@ -117,6 +117,16 @@ class ListingController extends Controller
     }
 
     /**
+     * Redirect 301
+     * @todo w. htaccess
+     */
+    public function tagClassicAction($slug)
+    {
+        return $this->redirect($this->generateUrl('UserDetail', array('slug' => $slug)));
+    }
+
+
+    /**
      * Tag listing w. minimal url or user detail
      * code beta
      */
@@ -125,10 +135,6 @@ class ListingController extends Controller
         $logger = $this->get('logger');
         $logger->info('*** tagAction');
         $logger->info('$slug = '.print_r($slug, true));
-
-        if (substr($slug, 0 , 1) == '@') {
-            return $this->redirect($this->generateUrl('UserDetail', array('slug' => substr($slug, 1))));
-        }
 
         return $this->redirect($this->generateUrl('ListingByTag', array('slug' => $slug)));
     }
