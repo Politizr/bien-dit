ee0b77278cf18f2e588e9401c5fe986967bacc47
Lionel Bouzonville <lionel.bouzonville@gmail.com>
2017-03-24 09:57:58 +0100

    Fix routing urls

--- src/Politizr/FrontBundle/Resources/public/js/politizr/xhr.js
+++ src/Politizr/FrontBundle/Resources/public/js/politizr/xhr.js
@@ -11,28 +11,28 @@ var RETURN_URL = 3;
 // ******************************************************** //
 
 // GENERAL
-var ROUTE_SHOW_HIDE_SUGGESTION = 'w/suggestion';
+var ROUTE_SHOW_HIDE_SUGGESTION = '$w/suggestion';
 
 // FOLLOW
-var ROUTE_FOLLOW_DEBATE = 'w/suivre/debat';
-var ROUTE_FOLLOW_USER = 'w/suivre/utilisateur';
-var ROUTE_FOLLOW_TAG = 'w/suivre/thematique';
-var ROUTE_FOLLOW_RELATIVE_DEBATE = 'w/suivre/debat/relatif';
+var ROUTE_FOLLOW_DEBATE = '$w/suivre/debat';
+var ROUTE_FOLLOW_USER = '$w/suivre/utilisateur';
+var ROUTE_FOLLOW_TAG = '$w/suivre/thematique';
+var ROUTE_FOLLOW_RELATIVE_DEBATE = '$w/suivre/debat/relatif';
 
 // COMMENTS
 var ROUTE_COMMENTS = 'commentaires';
-var ROUTE_COMMENT_CREATE = 'w/commentaire/nouveau';
+var ROUTE_COMMENT_CREATE = '$w/commentaire/nouveau';
 
 // DOCUMENTS
-var ROUTE_DOCUMENT_PHOTO_UPLOAD = 'w/document/photo/upload';
-var ROUTE_DOCUMENT_PHOTO_DELETE = 'w/document/photo/delete';
+var ROUTE_DOCUMENT_PHOTO_UPLOAD = '$w/document/photo/upload';
+var ROUTE_DOCUMENT_PHOTO_DELETE = '$w/document/photo/delete';
 
-var ROUTE_DOCUMENT_LISTING_MY_DRAFTS = 'w/mes-documents/brouillons';
-var ROUTE_DOCUMENT_LISTING_MY_BOOKMARKS = 'w/mes-favoris';
-var ROUTE_DOCUMENT_LISTING_USER_PUBLICATIONS = 'w/publications';
+var ROUTE_DOCUMENT_LISTING_MY_DRAFTS = '$w/mes-documents/brouillons';
+var ROUTE_DOCUMENT_LISTING_MY_BOOKMARKS = '$w/mes-favoris';
+var ROUTE_DOCUMENT_LISTING_USER_PUBLICATIONS = '$w/publications';
 
 var ROUTE_DOCUMENT_LISTING_TOP = 'documents/top';
-var ROUTE_DOCUMENT_LISTING_SUGGESTION = '/w/documents/suggestions';
+var ROUTE_DOCUMENT_LISTING_SUGGESTION = '/$w/documents/suggestions';
 
 var ROUTE_DOCUMENT_LISTING_RECOMMEND = 'documents/recommend';
 var ROUTE_DOCUMENT_LISTING_RECOMMEND_NAV = 'documents/recommend/nav';
@@ -44,38 +44,38 @@ var ROUTE_MAP_MENU = 'carte/menu';
 var ROUTE_MAP_BREADCRUMB = 'carte/breadcrumb';
 var ROUTE_MAP_SCHEMA = 'carte/schema';
 
-var ROUTE_DOCUMENT_BOOKMARK = 'w/document/bookmark';
+var ROUTE_DOCUMENT_BOOKMARK = '$w/document/bookmark';
 
 
 // DEBATES
-var ROUTE_DEBATE_UPDATE = 'w/debat/update';
-var ROUTE_DEBATE_PUBLISH = 'w/debat/publier';
-var ROUTE_DEBATE_DELETE = 'w/debat/supprimer';
-var ROUTE_DEBATE_PHOTO_INFO_UPDATE = 'w/debat/infos/photo/update';
+var ROUTE_DEBATE_UPDATE = '$w/debat/update';
+var ROUTE_DEBATE_PUBLISH = '$w/debat/publier';
+var ROUTE_DEBATE_DELETE = '$w/debat/supprimer';
+var ROUTE_DEBATE_PHOTO_INFO_UPDATE = '$w/debat/infos/photo/update';
 
 // REACTIONS
-var ROUTE_REACTION_UPDATE = 'w/reaction/update';
-var ROUTE_REACTION_PUBLISH = 'w/reaction/publier';
-var ROUTE_REACTION_DELETE = 'w/reaction/supprimer';
-var ROUTE_REACTION_PHOTO_INFO_UPDATE = 'w/reaction/infos/photo/update';
+var ROUTE_REACTION_UPDATE = '$w/reaction/update';
+var ROUTE_REACTION_PUBLISH = '$w/reaction/publier';
+var ROUTE_REACTION_DELETE = '$w/reaction/supprimer';
+var ROUTE_REACTION_PHOTO_INFO_UPDATE = '$w/reaction/infos/photo/update';
 
 var ROUTE_REACTION_LISTING_DEBATE_FIRST_CHILD = 'reactions/debat';
 
 
 // NOTIFICATIONS
-var ROUTE_NOTIF_LOADING = 'w/notif/chargement';
-var ROUTE_NOTIF_CHECK = 'w/notif/marque-vu';
-var ROUTE_NOTIF_CHECK_ALL = 'w/notif/tout-marque-vu';
-var ROUTE_NOTIF_EMAIL_SUBSCRIBE = 'w/notif/email/activer';
-var ROUTE_NOTIF_EMAIL_UNSUBSCRIBE = 'w/notif/email/desactiver';
-var ROUTE_NOTIF_CONTEXT_DEBATE_SUBSCRIBE = 'w/notif/debat/contexte/activer';
-var ROUTE_NOTIF_CONTEXT_DEBATE_UNSUBSCRIBE = 'w/notif/debat/contexte/desactiver';
-var ROUTE_NOTIF_CONTEXT_USER_SUBSCRIBE = 'w/notif/utilisateur/contexte/activer';
-var ROUTE_NOTIF_CONTEXT_USER_UNSUBSCRIBE = 'w/notif/utilisateur/contexte/desactiver';
+var ROUTE_NOTIF_LOADING = '$w/notif/chargement';
+var ROUTE_NOTIF_CHECK = '$w/notif/marque-vu';
+var ROUTE_NOTIF_CHECK_ALL = '$w/notif/tout-marque-vu';
+var ROUTE_NOTIF_EMAIL_SUBSCRIBE = '$w/notif/email/activer';
+var ROUTE_NOTIF_EMAIL_UNSUBSCRIBE = '$w/notif/email/desactiver';
+var ROUTE_NOTIF_CONTEXT_DEBATE_SUBSCRIBE = '$w/notif/debat/contexte/activer';
+var ROUTE_NOTIF_CONTEXT_DEBATE_UNSUBSCRIBE = '$w/notif/debat/contexte/desactiver';
+var ROUTE_NOTIF_CONTEXT_USER_SUBSCRIBE = '$w/notif/utilisateur/contexte/activer';
+var ROUTE_NOTIF_CONTEXT_USER_UNSUBSCRIBE = '$w/notif/utilisateur/contexte/desactiver';
 
 // REPUTATION COUNTER
-var ROUTE_SCORE_COUNTER = 'w/reputation/score';
-var ROUTE_BADGES_COUNTER = 'w/reputation/badges';
+var ROUTE_SCORE_COUNTER = '$w/reputation/score';
+var ROUTE_BADGES_COUNTER = '$w/reputation/badges';
 
 // MODAL
 var ROUTE_MODAL_HELP_US = 'politizr-needs-you';
@@ -88,27 +88,27 @@ var ROUTE_MODAL_CHARTE = 'charte-politizr';
 var ROUTE_OP_CLOSE = 'op/close';
 
 // NOTATION
-var ROUTE_NOTE = 'w/noter';
+var ROUTE_NOTE = '$w/noter';
 
 // SECURITY
 var ROUTE_SECURITY_LOGIN = 'login';
 var ROUTE_SECURITY_LOST_PASSWORD_CHECK = 'password/init';
-var ROUTE_SECURITY_PAYMENT_PROCESS = 'v/paiement';
+var ROUTE_SECURITY_PAYMENT_PROCESS = '$v/paiement';
 
 // TAGS
-var ROUTE_TAG_DEBATE_CREATE = 'w/debat/tag/create';
-var ROUTE_TAG_DEBATE_DELETE = 'w/debat/tag/delete';
-var ROUTE_TAG_REACTION_CREATE = 'w/reaction/tag/create';
-var ROUTE_TAG_REACTION_DELETE = 'w/reaction/tag/delete';
-var ROUTE_TAG_USER_CREATE = 'w/utilisateur/tag/create';
-var ROUTE_TAG_USER_DELETE = 'w/utilisateur/tag/delete';
-var ROUTE_TAG_USER_HIDE = 'w/utilisateur/tag/hide';
-var ROUTE_TAG_USER_ASSOCIATE = 'w/utilisateur/tag/associate';
+var ROUTE_TAG_DEBATE_CREATE = '$w/debat/tag/create';
+var ROUTE_TAG_DEBATE_DELETE = '$w/debat/tag/delete';
+var ROUTE_TAG_REACTION_CREATE = '$w/reaction/tag/create';
+var ROUTE_TAG_REACTION_DELETE = '$w/reaction/tag/delete';
+var ROUTE_TAG_USER_CREATE = '$w/utilisateur/tag/create';
+var ROUTE_TAG_USER_DELETE = '$w/utilisateur/tag/delete';
+var ROUTE_TAG_USER_HIDE = '$w/utilisateur/tag/hide';
+var ROUTE_TAG_USER_ASSOCIATE = '$w/utilisateur/tag/associate';
 
 var ROUTE_TAG_LISTING_TOP = 'tags/top';
 var ROUTE_TAG_LISTING_TYPE = 'tags/type';
 var ROUTE_TAG_LISTING_FAMILY = 'tags/family';
-var ROUTE_TAG_LISTING_USER = 'w/tags/user';
+var ROUTE_TAG_LISTING_USER = '$w/tags/user';
 var ROUTE_TAG_LISTING = 'tags/listing';
 
 // ORGANIZATION
@@ -119,19 +119,19 @@ var ROUTE_ORGANIZATION_DOCUMENT_LISTING = 'organisation/documents/listing';
 var ROUTE_ORGANIZATION_USER_LISTING = 'organisation/profils/listing';
 
 // USER
-var ROUTE_USER_MANDATE_CREATE = 'w/utilisateur/mandat/create';
-var ROUTE_USER_MANDATE_UPDATE = 'w/utilisateur/mandat/update';
-var ROUTE_USER_MANDATE_DELETE = 'w/utilisateur/mandat/delete';
-var ROUTE_USER_PROFILE_UPDATE = 'w/utilisateur/update';
-var ROUTE_USER_ORGA_UPDATE = 'w/utilisateur/orga/update';
-var ROUTE_USER_AFFINITIES_UPDATE = 'w/utilisateur/affinitees/update';
-var ROUTE_USER_PERSO_UPDATE = 'w/utilisateur/perso/update';
+var ROUTE_USER_MANDATE_CREATE = '$w/utilisateur/mandat/create';
+var ROUTE_USER_MANDATE_UPDATE = '$w/utilisateur/mandat/update';
+var ROUTE_USER_MANDATE_DELETE = '$w/utilisateur/mandat/delete';
+var ROUTE_USER_PROFILE_UPDATE = '$w/utilisateur/update';
+var ROUTE_USER_ORGA_UPDATE = '$w/utilisateur/orga/update';
+var ROUTE_USER_AFFINITIES_UPDATE = '$w/utilisateur/affinitees/update';
+var ROUTE_USER_PERSO_UPDATE = '$w/utilisateur/perso/update';
 
 var ROUTE_USER_VALIDATE_ID = 'utilisateur/validation/carte-identite';
 var ROUTE_USER_VALIDATE_PHOTO_UPLOAD = 'utilisateur/validation/photo-id-upload';
 
-var ROUTE_USER_PHOTO_UPLOAD = 'w/photo/upload';
-var ROUTE_USER_PHOTO_DELETE = 'w/photo/delete';
+var ROUTE_USER_PHOTO_UPLOAD = '$w/photo/upload';
+var ROUTE_USER_PHOTO_DELETE = '$w/photo/delete';
 
 
 var ROUTE_USER_LISTING_LAST_DEBATE_FOLLOWERS = 'debat/derniers-abonnes';
@@ -150,14 +150,14 @@ var ROUTE_USER_LISTING_USER_SUBSCRIBERS_CONTENT = 'auteur/content/derniers-abonn
 var ROUTE_USER_LISTING_BADGES = 'auteur/badges';
 
 // TIMELINE
-var ROUTE_TIMELINE_MINE = 'w/timeline';
+var ROUTE_TIMELINE_MINE = '$w/timeline';
 var ROUTE_TIMELINE_USER = 'user/timeline';
 
 // MONITORING
-var ROUTE_MONITORING_ABUSE = 'w/signaler-un-abus';
-var ROUTE_MONITORING_ABUSE_CHECK = 'w/signaler-un-abus/check';
-var ROUTE_MONITORING_ASK_FOR_UPDATE = 'w/modifier-mes-donnees';
-var ROUTE_MONITORING_ASK_FOR_UPDATE_CHECK = 'w/modifier-mes-donnees/check';
+var ROUTE_MONITORING_ABUSE = '$w/signaler-un-abus';
+var ROUTE_MONITORING_ABUSE_CHECK = '$w/signaler-un-abus/check';
+var ROUTE_MONITORING_ASK_FOR_UPDATE = '$w/modifier-mes-donnees';
+var ROUTE_MONITORING_ASK_FOR_UPDATE_CHECK = '$w/modifier-mes-donnees/check';
 
 // BUBBLES
 var ROUTE_BUBBLE_USER = 'bubble/user';
