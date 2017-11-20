2a06af9ea84a76b4adeca50fbd89f06f544d8271
Lionel Bouzonville <lionel.bouzonville@gmail.com>
2017-11-20 12:24:27 +0100

    minor fix

--- src/Politizr/FrontBundle/Resources/public/js/politizr/pages/menuCircle.js
+++ src/Politizr/FrontBundle/Resources/public/js/politizr/pages/menuCircle.js
@@ -1,29 +1,6 @@
 // beta
 $(function() {
-    $.when(
-        typeTagListing(
-            $('.sidebarTypeTags').find('.tagList').first(),
-            $('.sidebarTypeTags').find('.ajaxLoader').first()
-        ),
-        // topTagListing(
-        //     $('.sidebarTopTags').find('.tagList').first(),
-        //     $('.sidebarTopTags').find('.ajaxLoader').first()
-        // ),
-        familyTagListing(
-            $('.sidebarFamilyTags').find('.tagList').first(),
-            $('.sidebarFamilyTags').find('.ajaxLoader').first()
-        ),
-        userTagListing(
-            $('.sidebarFollowedTags').find('.tagList').first(),
-            $('.sidebarFollowedTags').find('.ajaxLoader').first()
-        ),
-        topDocumentListing(
-            $('.sidebarTopPosts').find('.documentList').first(),
-            $('.sidebarTopPosts').find('.ajaxLoader').first()
-        )
-    ).done(function(r1, r2, r3, r4) {
-        stickySidebar();
-    });
+    stickySidebar();
 });
 
 // Join circle
