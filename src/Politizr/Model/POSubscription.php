f06bac324ce71a103a87360c0a4216f379521be9
Lionel Bouzonville <lionel.bouzonville@gmail.com>
2019-07-31 19:52:45 +0200

    MySQL 5.7 / original "makeSlugUnique" throws Syntax error or access violation: 1055 Expression #1 of SELECT list is not in GROUP BY clause and contains nonaggregated column

--- src/Politizr/Model/POSubscription.php
+++ src/Politizr/Model/POSubscription.php
@@ -20,6 +20,22 @@ class POSubscription extends BasePOSubscription
     }
 
     /**
+     * Overide to manage update published doc without updating slug
+     * Overwrite to fully compatible MySQL 5.7
+     * note: original "makeSlugUnique" throws Syntax error or access violation: 1055 Expression #1 of SELECT list is not in GROUP BY clause and contains nonaggregated column
+     *
+     * @see BasePDDebate::createSlug
+     */
+    protected function createSlug()
+    {
+        $slug = $this->createRawSlug();
+        $slug = $this->limitSlugSize($slug);
+        $slug = $slug . '-' . uniqid();
+
+        return $slug;
+    }
+
+    /**
      * Format title & price
      *
      * @return string
