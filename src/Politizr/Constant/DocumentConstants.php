7e6a5d462ce05e42b8033f5b29cd955663acf93d
Lionel Bouzonville <lionel.bouzonville@gmail.com>
2021-09-01 18:27:03 +0200

    Fix #117

--- src/Politizr/Constant/DocumentConstants.php
+++ src/Politizr/Constant/DocumentConstants.php
@@ -29,6 +29,7 @@ class DocumentConstants
     const REASON_USER_OPERATION = 8;
     const REASON_OWNER_OPERATION = 9;
     const REASON_NO_REPUTATION = 10;
+    const REASON_OWNER = 12;
 
     // ******************************************************** //
     //                   NOTATION REASONS                       //
